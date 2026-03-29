b513c9c293128e0b1b3d52a4501c4aa22870babf
Yoel Zeldes <yoel.zeldes@gmail.com>
2026-03-29 16:42:56 -0700

    remove support for MPS, as it's slower than CPU

--- pruning_utils.py
+++ pruning_utils.py
@@ -68,9 +68,7 @@ TABARENA_NAME_TO_TASK_ID = {
 
 
 def get_device():
-    if torch.backends.mps.is_available():
-        return torch.device("mps")
-    elif torch.cuda.is_available():
+    if torch.cuda.is_available():
         return torch.device("cuda")
     return torch.device("cpu")
 
