7efe9bcbd0c6efe527a51c93d794eccbd6eb18d5
Yoel Zeldes <yoel.zeldes@gmail.com>
2026-09-03 22:11:09 -0700

    change default dataset

--- sweep_pipeline_layers.py
+++ sweep_pipeline_layers.py
@@ -210,7 +210,7 @@ def run_dataset(args, dataset):
 
 def main():
     parser = argparse.ArgumentParser(description="Sweep pipeline over multiple layers")
-    parser.add_argument("--dataset", type=str, nargs='+', default=["breast_cancer"])
+    parser.add_argument("--dataset", type=str, nargs='+', default=["tabarena"])
     parser.add_argument("--student_n", type=parse_student_n, nargs='+',
                         default=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9], help="Student training sizes")
     parser.add_argument("--layers", type=int, nargs='+', default=[23],
